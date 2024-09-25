59ba216bb51fae08234112060894ffeec4b94667
mimi1595O <158983247+mimi1595O@users.noreply.github.com>
2024-09-25 13:32:51 +0700

    Draft and outline
    push/commit ใน main file ชื่อ zeroui.py (ยังไม่ได้ upload)

---
+++ zerouidraft_outline.py
@@ -0,0 +1,17 @@
+import tkinter
+#import backend
+
+def selectionmenu():
+    """ for loop in backend
+        do create menu """
+    #backend.selectionmenus(y/n)
+def inputfield():
+    #input int/str
+    #validation
+    #error window
+    #backend.input()
+def outputdisplay():
+    #backend.output()
+    #error window()
+def errorwindow()
+    #error display (new windows)
